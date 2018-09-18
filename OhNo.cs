4c46fdea2b1b8533b12ff84cd2978b67d5b7a361
realityquintupled <40674794+realityquintupled@users.noreply.github.com>
2018-09-18 16:46:06 -0400

    hmmmmmmmm

---
+++ OhNo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Crash_On_Miss {
+    class OhNo {
+        [DllImport("ntdll.dll")]
+        public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+
+        [DllImport("ntdll.dll")]
+        public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+
+        static unsafe OhNo() {
+            Boolean t1;
+            uint t2;
+            RtlAdjustPrivilege(19, true, false, out t1);
+            NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out t2);
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,42 @@
+﻿using IllusionPlugin;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Crash_On_Miss {
+    public class Plugin : IPlugin {
+        public string Name => "Crash On Miss";
+        public string Version => "0.0.1";
+
+        public BeatmapObjectSpawnController Resouces { get; private set; }
+
+        public void OnApplicationStart() {
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        }
+
+        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1) {
+            BeatmapObjectSpawnController[] beatmapObjectSpawnControllers = Resources.FindObjectsOfTypeAll<BeatmapObjectSpawnController>();
+            BeatmapObjectSpawnController beatmapObjectSpawnController = beatmapObjectSpawnControllers.Length > 0 ? beatmapObjectSpawnControllers?[0] : null;
+            if(beatmapObjectSpawnController != null) {
+                beatmapObjectSpawnController.noteWasMissedEvent += delegate (BeatmapObjectSpawnController beatmapObjectSpawnController2, NoteController noteController) { if (noteController.noteData.noteType != NoteType.Bomb) new OhNo(); };
+                beatmapObjectSpawnController.noteWasCutEvent += delegate (BeatmapObjectSpawnController beatmapObjectSpawnController2, NoteController noteController, NoteCutInfo noteCutInfo) { if (!noteCutInfo.allIsOK) new OhNo(); };
+            }
+        }
+
+        public void OnApplicationQuit() {
+            SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+        }
+
+        public void OnLevelWasLoaded(int level) {
+
+        }
+
+        public void OnLevelWasInitialized(int level) {
+        }
+
+        public void OnUpdate() {
+        }
+
+        public void OnFixedUpdate() {
+        }
+    }
+}
